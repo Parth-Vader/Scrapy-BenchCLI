1c7ce9dec085b7a9bde5be145c11e7aa75506ffc
Parth-vader <vermaparth97@gmail.com>
2017-05-22 15:08:09 +0530

    spider changed : added colorful output at the end, AvSpeed.txt is no longer made

--- bench.py
+++ bench.py
@@ -7,10 +7,10 @@ def cli(long):
 	"""A tool for benchmarking your scrapy"""
 	if long == 1:
 		#subprocess.Popen(["/home/parth/gsoc/cli/venv/bin/scrapy" ,"crawl" ,"followall","&&","cat","AvSpeed.txt"], shell=True)
-		subprocess.Popen("/home/parth/gsoc/cli/venv/bin/scrapy crawl followall",shell=True)
+		subprocess.Popen("/home/parth/gsoc/cli/venv/bin/scrapy crawl followall -o items.csv",shell=True)
 		
 		#subprocess.Popen([])
 
 	elif long >1:
-		str = "for i in `seq 1 " + str(long) +" `; do scrapy crawl followall -o items.csv && mv AvSpeed.txt files/$i.txt; done"
-		subprocess.Popen(str.split())
+		arg = "for i in `seq 1 " + str(long) +" `; do scrapy crawl followall -o items.csv; done"
+		subprocess.Popen(arg,shell=True)
