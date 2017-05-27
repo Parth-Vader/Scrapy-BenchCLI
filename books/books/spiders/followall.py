eab98e216885d2fca9d112e358f43fe1869c3aa7
Parth-vader <vermaparth97@gmail.com>
2017-05-27 20:46:46 +0530

    Code made to follow PEP-8

--- books/books/spiders/followall.py
+++ books/books/spiders/followall.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-# -*- coding: utf-8 -*-   
+# -*- coding: utf-8 -*-
 import re
 from six.moves.urllib.parse import urlparse
 import datetime
@@ -9,6 +9,7 @@ from scrapy.linkextractors import LinkExtractor
 import click
 from books.items import Page
 fix = datetime.datetime.now()
+
 
 class FollowAllSpider(scrapy.Spider):
 
@@ -23,7 +24,7 @@ class FollowAllSpider(scrapy.Spider):
 
     def __init__(self, **kw):
         super(FollowAllSpider, self).__init__(**kw)
-        url = kw.get('url') or kw.get('domain') or 'http://localhost/books.toscrape.com/index.html'
+        url = 'http://localhost/books.toscrape.com/index.html'
         if not url.startswith('http://') and not url.startswith('https://'):
             url = 'http://%s/' % url
         self.url = url
@@ -33,73 +34,55 @@ class FollowAllSpider(scrapy.Spider):
         self.previtem = 0
         self.items = 0
         self.timesec = datetime.datetime.now()
+
     def start_requests(self):
         return [Request(self.url, callback=self.parse, dont_filter=True)]
 
     def parse(self, response):
-        """Parse a PageItem and all requests to follow
-
-        
-        @url http://www.scrapinghub.com/
-        @returns items 1 1
-        @returns requests 1
-        @scrapes url title foo
-        """        
+        """Parse a PageItem and all requests to follow"""
         page = self._get_item(response)
         r = [page]
-        
+
         r.extend(self._extract_requests(response))
         self.items = self.crawler.stats.get_value('item_scraped_count', 0)
         pages = self.crawler.stats.get_value('response_received_count', 0)
         a = self.crawler.stats.get_value('start_time')
         b = datetime.datetime.now()
-        c = b - datetime.timedelta(0,19800) # Done because my machine has a time zone problem
-        #newitem = items - self.previtem
-        #self.previtem = items
-        #print(newitem)
-        #fix = c
-        self.timesec = c-a
-        
+        # Done because my machine has a time zone problem
+        c = b - datetime.timedelta(0, 19800)
+        self.timesec = c - a
+
         if 280 <= self.items <= 300:
-        	t = datetime.datetime.now()
-        	global fix 
-        	fix = t - datetime.timedelta(0,19800)
-        	#print("here")
-        	#print(fix)
+            t = datetime.datetime.now()
+            global fix
+            fix = t - datetime.timedelta(0, 19800)
         if self.items > 300:
-        	self.items = self.items - 300
-        	self.timesec = c - fix
-        	'''print(items)
-        	print(a)
-        	print(b)
-        	print(c) 
-        	print("  ")
-        	print(fix)
-        	'''
-        
+            self.items = self.items - 300
+            self.timesec = c - fix
+
         return r
-    
+
     def close(self, reason):
-        f=open("AvSpeed.txt",'a')
-        f.write(" {0}".format((self.items * (1/self.timesec.total_seconds()))))
-        click.secho("\nThe average speed of the spider is {0} items/sec\n".format(self.items * (1/self.timesec.total_seconds())), fg='white',bold=True)
-    
+        f = open("AvSpeed.txt", 'a')
+        f.write(" {0}".format((self.items * (1 / self.timesec.total_seconds()))))
+        click.secho("\nThe average speed of the spider is {0} items/sec\n".format(
+            self.items * (1 / self.timesec.total_seconds())), fg='white', bold=True)
+
     def _get_item(self, response):
         item = Page(
             url=response.url,
             size=str(len(response.body)),
             referer=response.request.headers.get('Referer'),
-            rating = response.css('p.star-rating::attr(class)').extract_first().split(' ')[-1],
-	        title = response.css('.product_main h1::text').extract_first(),
-	        price = response.css('.product_main p.price_color::text').re_first('£(.*)'),
-	        stock = ''.join(response.css('.product_main .instock.availability ::text').re('(\d+)')),
-	        category = ''.join(response.css('ul.breadcrumb li:nth-last-child(2) ::text').extract()).strip(),
+            rating=response.css('p.star-rating::attr(class)').extract_first().split(' ')[-1],
+            title=response.css('.product_main h1::text').extract_first(),
+            price=response.css('.product_main p.price_color::text').re_first('£(.*)'),
+            stock=''.join(response.css('.product_main .instock.availability ::text').re('(\d+)')),
+            category=''.join(response.css('ul.breadcrumb li:nth-last-child(2) ::text').extract()).strip(),
         )
-        
+
         self._set_new_cookies(item, response)
         return item
 
-        
     def _extract_requests(self, response):
         r = []
         if isinstance(response, HtmlResponse):
