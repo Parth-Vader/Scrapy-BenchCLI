3528b3647244fd43d2f706abfbbbeac43d3e3427
Parth-vader <vermaparth97@gmail.com>
2017-05-27 20:46:46 +0530

    Code made to follow PEP-8

--- setup.py
+++ setup.py
@@ -1,18 +1,18 @@
 from setuptools import setup
 
 setup(
-	name='Bench',
-	version='1.0',
-	py_modules=['scrapy-bench'],
-	install_requires=[
-	'Click',
-	'scrapy',
-	'statistics',
-    'six',
-    'datetime',
-	],
-	entry_points='''
+        name='Bench',
+        version='1.0',
+        py_modules=['scrapy-bench'],
+        install_requires=[
+            'Click',
+            'scrapy',
+            'statistics',
+            'six',
+            'datetime',
+        ],
+        entry_points='''
 		[console_scripts]
 		scrapy-bench=bench:cli
 		''',
-)		
+)
